954c0b2d6fa99de7cd3a638301937439a909abe5
hiroCat <miki2189@gmail.com>
2016-06-26 04:18:55 +0200

    Angular frontend ( testing angular for the first time , not working )

---
+++ RockPaperScissorsAJ/RockPaperScissorsAJ/Scripts/_references.js
@@ -0,0 +1,6 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="angular.js" />
+/// <reference path="angular-mocks.js" />
+/// <reference path="angular-ui/ui-bootstrap.min.js" />
+/// <reference path="angular-ui/ui-bootstrap-tpls.min.js" />
+/// <reference path="gameapp/app.js" />

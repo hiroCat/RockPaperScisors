9dca9dc97c8b8747bff464b2a6aae67c30efb05a
Miquel Puigdomenech <miki2189@gmail.com>
2018-05-27 20:08:07 +0200

    -  old projects

--- RockPaperScissorsAJ/RockPaperScissorsAJ/Scripts/_references.js
+++
@@ -1,6 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="angular.js" />
-/// <reference path="angular-mocks.js" />
-/// <reference path="angular-ui/ui-bootstrap.min.js" />
-/// <reference path="angular-ui/ui-bootstrap-tpls.min.js" />
-/// <reference path="gameapp/app.js" />
